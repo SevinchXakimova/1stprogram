8e6d6b476ef66c996daa41a569b385a993702f38
Sevinch <s.xakimova@gmail.com>
2025-01-17 12:25:27 +0500

    my first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, how old are you"<< endl;
+    cin >> age;
+    cout << "so your age is: " <<age<< endl;
+    return 0;
+}
